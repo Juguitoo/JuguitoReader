fbf09aa968871859a126f8152f4a9967e5a1b7ce
Hugo Juan Gomez <hugojuan09082004@gmail.com>
2026-08-30 23:00:42 +0200

    fix: READER-004 reset reading data when EPUB changes

--- app/src/main/java/com/juguito/juguitoreader/domain/usecase/book/UpdateBookUseCase.kt
+++ app/src/main/java/com/juguito/juguitoreader/domain/usecase/book/UpdateBookUseCase.kt
@@ -21,16 +21,21 @@ class UpdateBookUseCase @Inject constructor(
         }
 
         return try {
+            val persistedBook = bookRepository.getBookById(book.id) ?: throw Exception()
+
             val folderIds = resolveFolderIds(book.folders, folderRepository)
             val genreIds = resolveGenreIds(book.genres, genreRepository)
+            if (persistedBook.localFilePath == book.localFilePath) {
 
-            bookRepository.updateBook(book)
 
-            bookRepository.syncCrossReferences(book.id, folderIds, genreIds)
+                bookRepository.updateBook(book)
 
+                bookRepository.syncCrossReferences(book.id, folderIds, genreIds)
+            } else {
+                bookRepository.updateBookWithNewEPUB(book, folderIds, genreIds)
+            }
             Result.success(Unit)
-        } catch (e: Exception) {
-            e.printStackTrace()
+        } catch (_: Exception) {
             Result.failure(JuguitoException(R.string.error_save_book))
         }
     }
