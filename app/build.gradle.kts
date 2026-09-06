c5edc76aa8e182ac1677d45eaad6ea1c41e8b141
Hugo Juan Gomez <hugojuan09082004@gmail.com>
2026-09-06 15:58:43 +0200

    chore: bump version to 1.2.0 (versionCode 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.juguito.juguitoreader"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
